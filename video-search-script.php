a77a4b3d7b8d8b71f1f68c4e529df64cc7897fd9
Toasty Reynolds <toasty@so.money>
2019-03-24 20:37:26 -0700

    Update video-search-script.php
    Added some error checking. Standardized on $VARIABLE for all user set variables, in case that helps people grasp structually.

--- video-search-script.php
+++ video-search-script.php
@@ -11,54 +11,77 @@
 
   //PROBABLY DON'T MESS WITH ANYTHING BELOW HERE UNLESS YOU'RE COOL WITH IT
   //_______________________________________________________________________
-  //CLEAN UP PATHS
-  if (strstr($SEARCH_DIRECTORY," ")) {
-    $SEARCH_DIRECTORY = str_replace(" ","\ ",$SEARCH_DIRECTORY);
+  //LET'S CHECK FROM TRAILING SLASHES IN OUR PATHS
+
+  // This is a very sloppy way to account for trailing slashes in the paths above.
+  if (substr($SEARCH_DIRECTORY, -1) == "/") { $SEARCH_DIRECTORY = substr($SEARCH_DIRECTORY, 0, -1); }
+
+  if (substr($LOG_DIRECTORY, -1) == "/") { $SEARCH_DIRECTORY = substr($SEARCH_DIRECTORY, 0, -1); }
+
+  //LET'S DO A LITTLE BIT OF ERROR CHECKING
+  //Check to see if the path set is a valid directory to search within
+  if (!is_dir($SEARCH_DIRECTORY)) { die("We don't seem to recognize ".$SEARCH_DIRECTORY." as a proper directory to search in.  We're so sorry. \n"); }
+  //Let's do some checking on our log directory.
+  if (is_dir($LOG_DIRECTORY) {
+    if (!is_writable($LOG_DIRECTORY)) {
+       die("We've found your log directory: ".$LOG_DIRECTORY.", but I'm afraid we can't write to it.\n");
+    }
+  } else {
+      die("We don't seem to recognize ".$LOG_DIRECTORY." as a proper directory to write logs to.  We're so sorry. \n");
   }
+
+  //This is a very sloppy way to support spaces in file names
+  if (strstr($SEARCH_DIRECTORY," ")) {
+    $ESC_SEARCH_DIRECTORY = str_replace(" ","\ ",$SEARCH_DIRECTORY);
+  } else {
+    $ESC_SEARCH_DIRECTORY = $SEARCH_DIRECTORY;
+  }
+
   if (strstr($LOG_DIRECTORY," ")) {
-    $LOG_DIRECTORY = str_replace(" ","\ ",$LOG_DIRECTORY);
+    $ESC_LOG_DIRECTORY = str_replace(" ","\ ",$LOG_DIRECTORY);
+  } else {
+    $ESC_SEARCH_DIRECTORY = $SEARCH_DIRECTORY;
   }
 
   //BUILD FIND COMMAND
-  $FIND_CMD = "find $SEARCH_DIRECTORY -type d -path '*/\.*' -prune -o -not -name '.*' -type f -print";
+  $FIND_CMD = "find $ESC_SEARCH_DIRECTORY -type d -path '*/\.*' -prune -o -not -name '.*' -type f -print";
   //echo ($FIND_CMD);
 
   //EXECUTE FIND COMMAND
   $FIND_RESULTS = shell_exec($FIND_CMD);
 
   //BUILD FILENAMES
-  $FILE_LIST = $LOG_DIRECTORY."/".$TIMESTAMP."_file_list.txt";
-  $SKIPPED_LIST = $LOG_DIRECTORY."/".$TIMESTAMP."_skipped_list.txt";
-  $CSV_LIST = $LOG_DIRECTORY."/".$TIMESTAMP."_format_list.csv";
+  $FILE_LIST = $ESC_LOG_DIRECTORY."/".$TIMESTAMP."_file_list.txt";
+  $SKIPPED_LIST = $ESC_LOG_DIRECTORY."/".$TIMESTAMP."_skipped_list.txt";
+  $CSV_LIST = $ESC_LOG_DIRECTORY."/".$TIMESTAMP."_format_list.csv";
   //WRITE RESULTS TO A FILE FOR SAFE KEEPING AND MEMORY MANAGEMENT
-
   $fp = fopen($FILE_LIST, 'w+');
   fwrite($fp, $FIND_RESULTS);
   fclose($fp);
   unset($fp);
 
-  $fp = fopen($CSV_LIST, 'w+');
-  fwrite($fp, "FILENAME, FILE PATH, FILE TYPE, FILE FORMAT, CODEC, VIDEO DETAILS, AUDIO FORMAT \n");
-  $handle = fopen($FILE_LIST, "r");
+  $FILE_POINTER = fopen($CSV_LIST, 'w+');
+  fwrite($FILE_POINTER, "FILENAME, FILE PATH, FILE TYPE, FILE FORMAT, CODEC, VIDEO DETAILS, AUDIO FORMAT \n");
+  $FILE_HANDLE = fopen($FILE_LIST, "r");
 
-  if ($handle) {
-      while (($line = fgets($handle)) !== false) {
+  if ($FILE_HANDLE) {
+      while (($LINE = fgets($FILE_HANDLE)) !== false) {
 
-        if (preg_match('/[\'^£$%&*()}{@#~?><>,|=+¬]/', $line))
+        if (preg_match('/[\'^£$%&*()}{@#~?><>,|=+¬]/', $LINE))
           {
             // one or more of the 'special characters' found in $string
-            $fsp = fopen($SKIPPED_LIST, 'a+');
-            fwrite($fsp, $line);
-            fclose($fsp);
-            unset($fsp);
+            $SKIPPED_LIST_POINTER = fopen($SKIPPED_LIST, 'a+');
+            fwrite($SKIPPED_LIST_POINTER, $LINE);
+            fclose($SKIPPED_LIST_POINTER);
+            unset($SKIPPED_LIST_POINTER);
             continue;
           }
 
           //RUN MEDIAINFO AGAINST THE FILE
-          if (strstr($line," ")) {
-            $FULL_FILE_PATH = str_replace(" ","\ ",$line);
+          if (strstr($LINE," ")) {
+            $FULL_FILE_PATH = str_replace(" ","\ ",$LINE);
           } else {
-            $FULL_FILE_PATH = $line;
+            $FULL_FILE_PATH = $LINE;
           }
 
           $MEDIAINFO_CMD = "/usr/local/bin/mediainfo --Output=XML ".$FULL_FILE_PATH;
@@ -68,16 +91,10 @@
 
           //PARSE MEDIA INFO RESULT
           $FILE_XML = new SimpleXMLElement($MEDIAINFO_RESULTS);
-
           //var_dump($FILE_XML);
-
           foreach ($FILE_XML->media->track as $track) {
               switch((string) $track['type']) { // Get attributes as element indices
               case 'General':
-                //if ($track->VideoCount < 1) {
-                //  continue;
-                //}
-
                 $FILE_TYPE = $track->FileExtension;
                 $FILE_FORMAT = $track->Format;
               break;
@@ -92,29 +109,22 @@
                   break;
               }
           }
-          echo $line;
+          echo $LINE;
           // /usr/local/bin/mediainfo
-          $FILE_NAME = trim(basename($line));
+          $FILE_NAME = trim(basename($LINE));
           $LINE_TO_WRITE = $FILE_NAME.",".trim($FULL_FILE_PATH).",".$FILE_TYPE.",".$FILE_FORMAT.",".$FILE_CODEC.",".$FILE_DETAILS.",".$FILE_AUDIO."\n";
-          fwrite($fp, $LINE_TO_WRITE);
-
+          fwrite($FILE_POINTER, $LINE_TO_WRITE);
       }
-
-
-      fclose($handle);
-
-
+      fclose($FILE_HANDLE);
   } else {
       // error opening the file.
       echo "COULD NOT OPEN FILE_LIST AT ".$FILE_LIST;
       echo "\n";
       exit(1);
-
   }
-  fclose($fp);
-  unset($fp);
-  unset($handle);
+  fclose($FILE_POINTER);
+  unset($FILE_POINTER);
+  unset($FILE_HANDLE);
   //CLEAN UP YOU FILTHY MONKEY
   exit(0);
-
 ?>
